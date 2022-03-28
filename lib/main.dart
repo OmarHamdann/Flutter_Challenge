0603e90509a8acb4d0fa2eca7743e34cf948372b
OmarHamdann <omar.haamdan@gmail.com>
2022-03-28 20:47:27 +0200

    invoke provider in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:sorting/provider/provider_notifer.dart';
 
+import 'login/login_screen.dart';
 import 'sorting.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -11,15 +14,16 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Sorting App',
-      theme: ThemeData(
-        primarySwatch: Colors.teal,
+    return ChangeNotifierProvider(
+      create: (BuildContext context) => LoginProvider(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Sorting App',
+        theme: ThemeData(
+          primarySwatch: Colors.teal,
+        ),
+        home: LoginScreen(),
       ),
-
-      home: const Sort(),
     );
   }
-
 }
