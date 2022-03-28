4b0a3e79692b45f0f547a97ba0c24d6808fdbf1b
OmarHamdann <omar.haamdan@gmail.com>
2022-03-28 20:49:28 +0200

    show user name in home screen and my defult button

--- lib/sorting.dart
+++ lib/sorting.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:sorting/algorithm.dart';
+import 'package:sorting/provider/provider_notifer.dart';
 import 'package:sorting/visualizer.dart';
+
+import 'components/reusable_widgets.dart';
 
 class Sort extends StatefulWidget {
   const Sort({Key? key}) : super(key: key);
@@ -23,8 +27,8 @@ class _SortState extends State<Sort> {
     var s1 = list
         .split(",")
         .where((x) {
-          return int.tryParse(x) != null;
-        })
+      return int.tryParse(x) != null;
+    })
         .toList()
         .map((x) => int.tryParse(x))
         .toList(); // s1.removeWhere((x){return x=='['||x==']';}); print(s1);
@@ -43,151 +47,194 @@ class _SortState extends State<Sort> {
       body: SingleChildScrollView(
         physics: const BouncingScrollPhysics(),
         child: Center(
-          child: Column(
-            children: [
-              const SizedBox(
-                height: 20,
-              ),
-
-              const SizedBox(
-                height: 20,
-              ),
-              SizedBox(
-                width: 150,
-                child: TextField(
-                  controller: data,
-                  onChanged: (e) {
-                    setState(() {});
-                  },
-                  maxLines: 3,
-                  minLines: 1,
-                  decoration: const InputDecoration(
-                      labelText: "Enter Data ...", hintText: "5,7,1,2,4,3,6"),
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const SizedBox(
+                  height: 10,
                 ),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  //////////////////////////////////sort
-                  ElevatedButton(
-                      onPressed: data.text.isNotEmpty
-                          ? () {
-                              setState(() {
-                                sortedArray = arrayData(data.text);
-                                sortedArray.sort();
-                                algorithm = SortAlgorithm();
-                                //////////////////////////////////insertion
+                Text(
+                  "HELLO ${context
+                      .read<LoginProvider>()
+                      .userName
+                      .toUpperCase()}",
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                defaultButton(
+                  onPressed: () {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(content:Text('HELLO ${context
+                            .read<LoginProvider>()
+                            .userName
+                            .toUpperCase()
+                            .toString()}'),backgroundColor: Colors.teal,));
+                  },
+                  child: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: const [
 
-                                timeInsertion =
-                                    algorithm.insertionSort(arrayData(data.text));
-                                //////////////////////////////////Bubble
-                                timeBubble =
-                                    algorithm.bubble(arrayData(data.text));
-                                //////////////////////////////////mergeSort
-                                timeMerge = algorithm.mergeSort(
-                                    arrayData(data.text),
-                                    0,
-                                    arrayData(data.text).length - 1);
-                                //////////////////////////////////heapSort
-                                timeHeap =
-                                    algorithm.heapSort(arrayData(data.text));
-                                //////////////////////////////////selectionSort
-                                timeSelection =
-                                    algorithm.heapSort(arrayData(data.text));
-                              }); //useEffect
-                            }
-                          : null,
-                      child: const Text("sorting")),
-                  const SizedBox(width: 23),
-                ],
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              if (sortedArray.isNotEmpty) Text("Sorting Array : $sortedArray"),
-              const SizedBox(
-                height: 20,
-              ),
-              SingleChildScrollView(
-                physics: const BouncingScrollPhysics(),
-                scrollDirection: Axis.horizontal,
-                child: DataTable(columns: const [
-                  DataColumn(label: Text('SortingType')),
-                  DataColumn(label: Text('ExecutionTime')),
-                  DataColumn(label: Text('TimeComplexity')),
+                      Text('ReusableButton  ',
+                        style: TextStyle(color: Colors.white),),
+                      Icon(Icons.print, color: Colors.white,),
+                    ],),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Expanded(
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: TextField(
+                            controller: data,
+                            onChanged: (e) {
+                              setState(() {});
+                            },
+                            maxLines: 3,
+                            minLines: 1,
+                            decoration: const InputDecoration(
+                                labelText: "Enter Data ...",
+                                hintText: "5,7,1,2,4,3,6"),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    //////////////////////////////////sort
+                    ElevatedButton(
+                        onPressed: data.text.isNotEmpty
+                            ? () {
+                          setState(() {
+                            sortedArray = arrayData(data.text);
+                            sortedArray.sort();
+                            algorithm = SortAlgorithm();
+                            //////////////////////////////////insertion
 
-                ], rows: [
-                  DataRow(
-                    cells: [
-                      const DataCell(
-                        Text('SelectionSort'),
-                      ),
-                      DataCell(
-                        Text('$timeSelection'),
-                      ),
-                      const DataCell(
-                        Text('O(n^2)'),
-                      ),
-                    ],
-                  ),
-                  DataRow(
-                    cells: [
-                      const DataCell(
-                        Text('InsertionSort'),
-                      ),
-                      DataCell(
-                        Text(timeInsertion.toString()),
-                      ),
-                      const DataCell(
-                        Text('O(n^2)'),
-                      ),
-                    ],
-                  ),
-                  DataRow(
-                    cells: [
-                      const DataCell(
-                        Text('MergeSort'),
-                      ),
-                      DataCell(
-                        Text(timeMerge.toString()),
-                      ),
-                      const DataCell(
-                        Text('O(n log(n))'),
-                      ),
-                    ],
-                  ),
-                  DataRow(
-                    cells: [
-                      const DataCell(
-                        Text('HeapSort'),
-                      ),
-                      DataCell(
-                        Text(timeHeap.toString()),
-                      ),
-                      const DataCell(
-                        Text('O(n log(n))'),
-                      ),
-                    ],
-                  ),
-                  DataRow(
-                    cells: [
-                      const DataCell(
-                        Text('BubbleSort'),
-                      ),
-                      DataCell(
-                        Text(timeBubble.toString()),
-                      ),
-                      const DataCell(
-                        Text('O(n^2)'),
-                      ),
-                    ],
-                  ),
-                ]),
-              ),
-            ],
+                            timeInsertion = algorithm
+                                .insertionSort(arrayData(data.text));
+                            //////////////////////////////////Bubble
+                            timeBubble =
+                                algorithm.bubble(arrayData(data.text));
+                            //////////////////////////////////mergeSort
+                            timeMerge = algorithm.mergeSort(
+                                arrayData(data.text),
+                                0,
+                                arrayData(data.text).length - 1);
+                            //////////////////////////////////heapSort
+                            timeHeap =
+                                algorithm.heapSort(arrayData(data.text));
+                            //////////////////////////////////selectionSort
+                            timeSelection =
+                                algorithm.heapSort(arrayData(data.text));
+                          }); //useEffect
+                        }
+                            : null,
+                        child: const Text("sorting")),
+                    const SizedBox(width: 23),
+                  ],
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                if (sortedArray.isNotEmpty)
+                  Text("Sorting Array : $sortedArray"),
+                const SizedBox(
+                  height: 20,
+                ),
+                SingleChildScrollView(
+                  physics: const BouncingScrollPhysics(),
+                  scrollDirection: Axis.horizontal,
+                  child: DataTable(columns: const [
+                    DataColumn(label: Text('SortingType')),
+                    DataColumn(label: Text('ExecutionTime')),
+                    DataColumn(label: Text('TimeComplexity')),
+                  ], rows: [
+                    DataRow(
+                      cells: [
+                        const DataCell(
+                          Text('SelectionSort'),
+                        ),
+                        DataCell(
+                          Text('$timeSelection'),
+                        ),
+                        const DataCell(
+                          Text('O(n^2)'),
+                        ),
+                      ],
+                    ),
+                    DataRow(
+                      cells: [
+                        const DataCell(
+                          Text('InsertionSort'),
+                        ),
+                        DataCell(
+                          Text(timeInsertion.toString()),
+                        ),
+                        const DataCell(
+                          Text('O(n^2)'),
+                        ),
+                      ],
+                    ),
+                    DataRow(
+                      cells: [
+                        const DataCell(
+                          Text('MergeSort'),
+                        ),
+                        DataCell(
+                          Text(timeMerge.toString()),
+                        ),
+                        const DataCell(
+                          Text('O(n log(n))'),
+                        ),
+                      ],
+                    ),
+                    DataRow(
+                      cells: [
+                        const DataCell(
+                          Text('HeapSort'),
+                        ),
+                        DataCell(
+                          Text(timeHeap.toString()),
+                        ),
+                        const DataCell(
+                          Text('O(n log(n))'),
+                        ),
+                      ],
+                    ),
+                    DataRow(
+                      cells: [
+                        const DataCell(
+                          Text('BubbleSort'),
+                        ),
+                        DataCell(
+                          Text(timeBubble.toString()),
+                        ),
+                        const DataCell(
+                          Text('O(n^2)'),
+                        ),
+                      ],
+                    ),
+                  ]),
+                ),
+                const SizedBox(
+                  height: 50,
+                ),
+              ],
+            ),
           ),
         ),
       ),
